4efa78d0b4fb84a65126d8cb5f353f1fb0b29267
Brat Wiekszy <bratwiekszy@gmail.com>
2017-11-22 19:06:01 +0100

    Removed wrong user model properties.

--- BlackGaugeContent/Models/AspUser.cs
+++ BlackGaugeContent/Models/AspUser.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
 namespace Bgc.Models
@@ -11,8 +10,6 @@ namespace Bgc.Models
 		public byte   GenderId  { get; set; }
 		public int    Respek    { get; set; }
 		public int    DogeCoins {get; set;}
-		
-		public int MemeSessionId {get; set;}
 
 		public Gender Gender { get; set; }
 		public ICollection<Blacklist>    Blacklisted { get; set; }
@@ -20,7 +17,6 @@ namespace Bgc.Models
 		public ICollection<Exclusive>    Exclusive { get; set; }
 		public ICollection<MemeComment>  MemeComments { get; set; }
 		public ICollection<MemeRating>   MemeRatings { get; set; }
-		[ForeignKey("MemeSessionId")]
 		public MemeUserSession           MemeSession {get; set;}
 	}
 }
