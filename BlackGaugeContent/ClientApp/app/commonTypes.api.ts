f600047001931ecdd1f8eba8b26a0058b78e6427
Brat Wiekszy <bratwiekszy@gmail.com>
2017-12-27 11:02:11 +0100

    Added chat data, which encapsulates chatter list, and hashset (intended for fast lookups). ChatData can be constructed from chat impulse array, as wel as comrade list. Added Chatter type, which encapsulates comrade data and messaging. ChatData is intended to be managed by impulseHandler, but lended to message componenet, which assures that all incoming message signals aren't lost when message page isn't active.

--- BlackGaugeContent/ClientApp/app/commonTypes.api.ts
+++ BlackGaugeContent/ClientApp/app/commonTypes.api.ts
@@ -4,3 +4,36 @@
 		public index: number) {
 	}
 }
+
+export class HashSet<T> {
+	private elements : {[index: string]: T};
+
+	public count: number;
+
+	constructor() {
+		this.elements = {} as { [index: string]: T };
+		this.count    = 0;
+	}
+
+	public hasKey(key: string): boolean {
+		return this.elements.hasOwnProperty(key);
+	}
+
+	public add(key: string, value: T) {
+		if (!this.elements.hasOwnProperty(key)) 
+			this.count ++;
+
+		this.elements[key] = value;
+	}
+
+	public item(key: string): T {
+		return this.elements[key];
+	}
+	
+	public remove(key: string): T {
+		let item = this.elements[key];
+		delete this.elements[key];
+		this.count --;
+		return item;
+	}
+}
---
+++ BlackGaugeContent/ClientApp/app/models/chatData.ts
@@ -0,0 +1,87 @@
+﻿import { IComradeEntry } from './users';
+import { ChatImpulse } from './signals';
+import { HashSet } from '../commonTypes.api';
+
+export class ChatData {
+	public  list:    Chatter[];
+	private hashset: HashSet<Chatter>;
+
+	constructor(impulses: ChatImpulse[]) {
+		this.hashset = new HashSet<Chatter>();
+
+		for (var impulse of impulses) {
+			let name    = impulse.comrade;
+			let chatter = new Chatter(name, impulse.impulses);
+			this.hashset.add(name, chatter);
+		}
+	}
+
+	/**
+	 * puts comrades as chatters in comrade list. 
+	 * @param comrades
+	 */
+	public zipComrades(comrades: IComradeEntry[]) {
+		if (!comrades)
+			return;
+		this.list = new Array<Chatter>(comrades.length);
+		for (let i = 0; i < comrades.length; i++) {
+			let name = comrades[i].name;
+			let chatter = this.hashset.item(name);
+
+			if (!chatter) {
+				chatter = new Chatter(name, 0);
+				this.hashset.add(name, chatter);
+			}
+			chatter.interactions = comrades[i].interactions;
+			this.list[i] = chatter;
+		}
+	}
+
+	public comradesLoaded(): boolean {
+		return this.list && this.list.length > 0;
+	}
+
+	/**
+	 * Checks, if chatter exists, if not, creates one with name and returns it.
+	 * @param name
+	 */
+	public getChatter(name: string): Chatter {
+		let chatter = this.hashset.item(name);
+		if (!chatter)
+			chatter = new Chatter(name, 0);
+		return chatter;
+	}
+}
+
+export class Chatter {
+	public messages: Message[];
+	public impulses: number;
+	public comrade:  string;
+	public interactions: number;
+	private onList:  boolean = false;
+
+	constructor(comrade: string, impulses: number) {
+		this.comrade      = comrade;
+		this.impulses     = impulses;
+		this.interactions = 0;
+		this.messages     = new Array<Message>();
+	}
+
+	zipIfNotIn(array: Chatter[]) {
+		if (this.onList)
+			return;
+		array.push(this);
+		this.onList = true;
+	}
+}
+
+export class Message {
+	public id:     number |null;
+	public userId: number |null;
+	public sent:   boolean|null;
+	public seen:   boolean|null;
+
+	public otherName: string|null;
+	public text:      string;
+	
+}
