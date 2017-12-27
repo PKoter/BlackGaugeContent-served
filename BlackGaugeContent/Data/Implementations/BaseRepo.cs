85f9d84dfb69afdbb3caaafd18cb1702b04b06b2
Brat Wiekszy <bratwiekszy@gmail.com>
2017-12-27 10:40:21 +0100

    Extended base repository to provide api for querying stored procedures.

--- BlackGaugeContent/Data/Implementations/BaseRepo.cs
+++ BlackGaugeContent/Data/Implementations/BaseRepo.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Bgc.Api;
 using Bgc.Data.Contracts;
 using Bgc.Development;
+using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bgc.Data.Implementations
@@ -69,6 +74,89 @@ namespace Bgc.Data.Implementations
 					"No valid user for {0} credential!", nameof(userId));
 			}
 			return result;
+		}
+
+		[NotNull]
+		protected DbCommand BuildSpCommand(string spName)
+		{
+			var command = _context.Database.GetDbConnection().CreateCommand();
+			command.CommandText = spName;
+			command.CommandType = CommandType.StoredProcedure;
+			return command;
+		}
+	}
+
+	internal static class SqlExtensions
+	{
+		/// <summary>
+		/// Executes stored procedure command and builds list of DTO. Caution: names of columns and fields must match.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="command"></param>
+		/// <returns></returns>
+		[ItemNotNull]
+		internal static async Task<List<T>> ToListAsync<T>([NotNull] this DbCommand command) where T : class, new()
+		{
+			var list = new List<T>();
+			using(command)
+			{
+				if((command.Connection.State & ConnectionState.Open) == 0)
+					command.Connection.Open();
+
+				using(var reader = await command.ExecuteReaderAsync())
+				{
+					var colNumber = reader.FieldCount;
+					var properties = new List<PropertyInfo>(colNumber);
+					var type = typeof(T);
+
+					for(int i = 0; i < colNumber; i++)
+					{
+						var colName = reader.GetName(i);
+						properties.Add(type.GetProperty(colName));
+					}
+
+					while(reader.Read())
+					{
+						var obj = new T();
+						for(int i = 0; i < colNumber; i++)
+						{
+							properties[i].SetValue(obj, reader[i]);
+						}
+
+						list.Add(obj);
+					}
+				}
+			}
+			return list;
+		}
+
+		/// <summary>
+		/// Executes stored procedure command and builds list of DTO. Map is used to cast column data into right properties.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="command"></param>
+		/// <param name="map"></param>
+		/// <returns></returns>
+		[ItemNotNull]
+		internal static async Task<List<T>> ToListAsync<T>([NotNull] this DbCommand command, Action<DbDataReader, T> map) where T : class, new()
+		{
+			var list = new List<T>();
+			using(command)
+			{
+				if((command.Connection.State & ConnectionState.Open) == 0)
+					command.Connection.Open();
+
+				using(var reader = await command.ExecuteReaderAsync())
+				{
+					while(reader.Read())
+					{
+						var obj = new T();
+						map(reader, obj);
+						list.Add(obj);
+					}
+				}
+			}
+			return list;
 		}
 	}
 }
